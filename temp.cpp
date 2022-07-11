eb6cdbb2a36543aba037f7f1c51ff2a87233e17f
syash7202 <workspacesyash7202@gmail.com>
2022-07-11 23:04:54 +0530

    Random Stuff

---
+++ temp.cpp
@@ -0,0 +1,115 @@
+ #include <iostream>  
+ using namespace std;
+
+ int main()
+ {
+
+
+//  Bitwise operators
+//    int a =10;
+//    int b=15;
+
+//    cout<<"a & b " << (a&b )<<endl;
+//    cout<<"a | b " << (a|b) <<endl;
+//    cout<<"~a  " << ~a  <<endl;
+//    cout<<"a ^ b " << (a^b) <<endl;
+    
+
+
+
+//  Sum from 1 to n
+    // int n, sum =0;
+    // cin>>n;
+
+
+    // for(int i= 1; i<=n; i++)
+    // {
+    //     sum+=i;
+       
+    // }
+
+    // cout<<"Sum upto "<<n<<" is = "<<sum << endl;
+
+
+
+
+// Fibonacci series
+
+// int n;
+// cin>>n;
+// int a=0,b=1;
+// cout<<a<<" "<<b;
+//  for(int i=0; i<+n; i++)
+//  {
+//     int next_num= a+b;
+//     cout<<" "<<next_num;
+//     a=b;
+//     b=next_num;
+//  }
+
+
+//  Prime number or not
+
+// int n;
+// cin>>n;
+
+// bool isPrime=1;
+
+// for (int i = 2; i <= n-1; i++)
+// {
+//     if (n%i==0)
+//     {
+//         isPrime=0;
+//         break;
+
+//     }
+  
+// }
+
+// if (isPrime ==0)
+// {
+//     cout<<"not prime"<<endl;
+// }
+       
+// else
+// {
+//     cout<<"Prime number"<<endl;
+// }
+
+// Difference of  product  and  sum of digits
+
+// int n;
+// cin>>n;
+// int prod=1,sum=0;
+// while (n!=0)
+// {
+//     int digit= n%10;
+//     prod=prod*digit;
+//     sum=sum+digit;
+//     n=n/10;
+// }
+
+// int answer = prod -sum;
+// cout<< answer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     return 0;
+
+ }
