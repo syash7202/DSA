f986a17bd6643867d71446e11a2abb43df154c74
syash7202 <workspacesyash7202@gmail.com>
2022-07-26 12:59:42 +0530

    Selection sort algo

---
+++ selection_sort.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+
+void selectionSort(int array[], int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+        int minIndex=i;
+        for (int j = i+1; j < size; j++)
+        {
+            if (array[minIndex]>array[j])
+            {
+                minIndex=j;
+            }
+            swap(array[i],array[minIndex]);
+        }
+        
+    }
+    
+}
+
+void printArray(int array[], int size)
+{
+    for(int i =0; i<size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    cout<<endl;
+}
+
+int main()
+{
+
+    int size;
+    cin>>size;
+
+    int array[size];
+    for(int i =0; i<size; i++)
+    {
+        array[i]=rand()%10;
+    }
+
+    printArray(array, size);
+    selectionSort(array,size);
+    printArray(array, size);
+
+    return 0;
+}
