1bd447526b67193d50af1c419efefe863b03397b
syash7202 <workspacesyash7202@gmail.com>
2022-07-29 22:16:19 +0530

    reverse word in array

---
+++ reverse_array_words.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+// t h e   s k y   i s   b l u e /0 
+
+
+
+
+
+
+
+void reverseWords(string s)
+{
+    
+}
+
+int main()
+{
+
+    string s;
+    s="the sky is blue";
+
+    reverseWords(s);
+    return 0;
+}
