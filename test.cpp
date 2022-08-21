9a4ba23db5575c15fadd77e84148e3b32eab5db2
syash7202 <workspacesyash7202@gmail.com>
2022-08-21 21:51:32 +0530

    test run

--- test.cpp
+++ test.cpp
@@ -46,3 +46,65 @@ using namespace std;
 
 //     return 0;
 // }
+
+// bikweekly contest 85
+
+// int main() //incomplete
+// {
+
+//     string blocks = "WBBWWBBWBW";
+//     int k = 7;
+
+//     int countB = 0;
+//     for (int i = 0; i < blocks.length(); i++)
+//     {
+//         if (blocks[i] == 'B')
+//         {
+//             countB++;
+//         }
+//     }
+//     cout << countB << endl;
+//     return 0;
+// }
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for (int i = 0; i < matrix.size(); i++) // transpose of a matrix
+    {
+        for (int j = i; j < matrix.size(); j++)
+        {
+            if (i != j)
+            {
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    int col = matrix[0].size();
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < col / 2; j++)
+        {
+            int temp = matrix[i][j];
+
+            matrix[i][j] = matrix[i][col - j - 1];
+            matrix[i][col - j - 1] = temp;
+        }
+    }
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix.size(); j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
