dcd7005392c5e55fca2ea5ee7407c6911518614c
syash7202 <workspacesyash7202@gmail.com>
2023-09-03 21:38:06 +0530

    climb stairs

---
+++ step_to_climb.cpp
@@ -0,0 +1,27 @@
+// Climb stairs problem at leetcode :https://leetcode.com/problems/climbing-stairs/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n)
+{
+    if (n <= 2)
+        return n;
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+}
+
+int main()
+{
+    int n = 20;
+    int ans = climbStairs(n);
+    cout << ans << endl;
+
+    return 0;
+}
