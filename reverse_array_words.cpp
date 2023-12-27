8b4d835a2de58f1ea104f4f2b4c042fad049a89f
syash7202 <workspacesyash7202@gmail.com>
2023-12-27 14:09:05 +0530

    revserve array of words

--- reverse_array_words.cpp
+++ reverse_array_words.cpp
@@ -3,25 +3,22 @@ using namespace std;
 // t h e   s k y   i s   b l u e /0 
 
 
-
-
-
-
-
 void reverseWords(string s)
 {
     int start=0;
-    int end =s.size()-1;
+    int end =s.length()-1;
     while(start<end)
     {
         swap(s[start],s[end]);
     }
+
 }
 
 int main()
 {
     string s;
     s="the sky is blue";
+    cout<<s<<endl;
 
     reverseWords(s);
 
